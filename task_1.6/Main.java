09e63db8f366afa1be1a5b5ad4b626c4ab96d88c
Nadezhdasha <74055045+Nadezhdasha@users.noreply.github.com>
2020-11-12 03:12:28 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,19 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+
+        public class Main {
+            public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int num1 = scan.nextInt();
+                double num2 = num1/100*15;
+                System.out.println(num1+num2);
+
+
+                public class Main {
+            public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int num1 = scan.nextInt();
+                double num2 = num1*15.0/100;
+                System.out.println(num1+num2);
