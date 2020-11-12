90c92c5cd3665b9185abfef84e49d811d37cdd47
Nadezhdasha <74055045+Nadezhdasha@users.noreply.github.com>
2020-11-12 03:09:07 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,15 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+   int num1 =5;
+   int num2 =5;
+
+   System.out.println(num1*num2);
+
+    }
+
+    }
